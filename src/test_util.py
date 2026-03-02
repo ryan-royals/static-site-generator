9ea8f1cd7cb79e5ee5fe48029b5212cd0b401f17
Ryan Royals <ryan.royals95@gmail.com>
2026-03-02 11:12:06 +1030

    c3l1

---
+++ src/test_util.py
@@ -0,0 +1,171 @@
+import unittest
+
+from textnode import TextNode, TextType
+from util import text_node_to_html_node, split_nodes_delimeter
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_not_valid_type(self):
+        node = TextNode("Not a valid Node", None)
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
+    def test_text(self):
+        text = TextNode("Text Node", TextType.TEXT)
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "Text Node")
+        self.assertEqual(html.props_to_html(), "")
+        self.assertEqual(html.tag, None)
+        self.assertEqual(html.value, "Text Node")
+
+    def test_bold(self):
+        text = TextNode("Bold Text Node", TextType.BOLD)
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "<b>Bold Text Node</b>")
+        self.assertEqual(html.props_to_html(), "")
+        self.assertEqual(html.tag, "b")
+        self.assertEqual(html.value, "Bold Text Node")
+
+    def test_italic(self):
+        text = TextNode("Italic Text Node", TextType.ITALIC)
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "<i>Italic Text Node</i>")
+        self.assertEqual(html.props_to_html(), "")
+        self.assertEqual(html.tag, "i")
+        self.assertEqual(html.value, "Italic Text Node")
+
+    def test_code(self):
+        text = TextNode("Code Text Node", TextType.CODE)
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "<code>Code Text Node</code>")
+        self.assertEqual(html.props_to_html(), "")
+        self.assertEqual(html.tag, "code")
+        self.assertEqual(html.value, "Code Text Node")
+
+    def test_link(self):
+        text = TextNode("Link Text Node", TextType.LINK,
+                        "https://ryanroyals.cloud")
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "<a>Link Text Node</a>")
+        self.assertEqual(html.props_to_html(),
+                         ' href="https://ryanroyals.cloud"')
+        self.assertEqual(html.tag, "a")
+        self.assertEqual(html.value, "Link Text Node")
+
+    def test_image(self):
+        text = TextNode("Image Text Node", TextType.IMAGE,
+                        "https://ryanroyals.cloud/cat.png")
+        html = text_node_to_html_node(text)
+
+        self.assertEqual(html.to_html(), "<img></img>")
+        self.assertEqual(html.props_to_html(),
+                         ' src="https://ryanroyals.cloud/cat.png" alt="Image Text Node"')
+        self.assertEqual(html.tag, "img")
+        self.assertEqual(html.value, "")
+
+
+class SplitNodesDelimeter(unittest.TestCase):
+    def test_invalid_target(self):
+        nodes = [
+            TextNode("*test", TextType.TEXT)
+        ]
+        with self.assertRaises(Exception):
+            split_nodes_delimeter(nodes, "*", TextType.ITALIC)
+
+    def test_short_target(self):
+        nodes = [
+            TextNode("*test*", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("test", TextType.ITALIC)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    def test_single_end_target(self):
+        nodes = [
+            TextNode("This is a *test*", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("test", TextType.ITALIC)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    def test_single_start_target(self):
+        nodes = [
+            TextNode("*This* is a test", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("This", TextType.ITALIC),
+            TextNode(" is a test", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    def test_middle_target(self):
+        nodes = [
+            TextNode("This is a *test* that splits in the middle", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("test", TextType.ITALIC),
+            TextNode(" that splits in the middle", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    def test_multi_node(self):
+        nodes = [
+            TextNode("This is a *test*", TextType.TEXT),
+            TextNode("This is another *test*", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("test", TextType.ITALIC),
+            TextNode("This is another ", TextType.TEXT),
+            TextNode("test", TextType.ITALIC)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    def test_multi_target(self):
+        nodes = [
+            TextNode(
+                "This is a *test* that splits *twice* in a tricky way", TextType.TEXT)
+        ]
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("test", TextType.ITALIC),
+            TextNode(" that splits ", TextType.TEXT),
+            TextNode("twice", TextType.ITALIC),
+            TextNode(" in a tricky way", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes_delimeter(
+            nodes, "*", TextType.ITALIC), expected)
+
+    # def test_multi_transform(self):
+    #     nodes = [
+    #         TextNode("This is a *test* that has `two` transforms", TextType.TEXT),
+    #     ]
+    #     expected = [
+    #         TextNode("This is a ", TextType.TEXT),
+    #         TextNode("test", TextType.ITALIC),
+    #         TextNode(" that has ", TextType.TEXT),
+    #         TextNode("two", TextType.CODE),
+    #         TextNode(" transforms", TextType.TEXT)
+    #     ]
+    #     pipelined = []
+    #
+    #     self.assertEqual(split_nodes_delimeter(
+    #         nodes, "*", TextType.ITALIC), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/util.py
@@ -0,0 +1,40 @@
+
+from leafnode import LeafNode
+from textnode import TextType, TextNode
+
+
+def split_nodes_delimeter(old_nodes, delimeter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            parts = node.text.split(delimeter)
+            if len(parts) % 2 == 0:
+                raise Exception(f"Invalid markdown: unmatched delimiter '{
+                    delimeter}' in '{node.text}'")
+            else:
+                for i, part in enumerate(parts):
+                    if part == "":
+                        continue
+                    node_type = TextType.TEXT if i % 2 == 0 else text_type
+                    new_nodes.append(TextNode(part, node_type))
+    return new_nodes
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("text_node not valid TextType type")
