dbb713a124387d95106bb0a12015724de2279de6
Ryan Royals <ryan.royals95@gmail.com>
2026-03-16 09:40:19 +1030

    c5l1

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
-from types.textnode import TextNode, TextType
+import os
+import shutil
 
-print("hello world")
+
+def files_from_dir(src, dest):
+    print(f"working on dir {src}")
+    files = os.listdir(src)
+    for file in files:
+        file_path = os.path.join(src, file)
+        if os.path.isfile(file_path):
+            print(f"copying {file_path}")
+            shutil.copy(file_path, dest)
+        else:
+            next_dir = os.path.join(src, file)
+            next_dest = os.path.join(dest, file)
+            print(f"making dir {next_dest}")
+            os.mkdir(next_dest)
+            files_from_dir(next_dir, next_dest)
 
 
 def main():
-    print(TextNode("anchor text", TextType.LINK, "https://www.boot.dev"))
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    os.mkdir("./public")
+    files_from_dir("./static", "./public")
 
 
 main()
